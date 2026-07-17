fe5c88ce7a645bd4f68a2343adb5898da11a164e
EliasH-E <hillmane@carleton.edu>
2026-07-17 10:01:42 -0500

    Added Repro Rate Data Node

--- source/sgp_mode/SGPWorldSetup.cc
+++ source/sgp_mode/SGPWorldSetup.cc
@@ -423,6 +423,8 @@ void SGPWorld::SetupHosts(long unsigned int* POP_SIZE) {
       // NOTE - Move env io assignment to different signal that is triggered on inject?
       // AssignNewEnvIO(new_sym->GetHardware().GetCPUState()); // Add to AddSymbiont
       // Set sym's parent task
+
+      //Try turning off
       if (task_env.IsSymTask(nand_task_id)) {
         new_sym->GetHardware().GetCPUState().SetParentTaskPerformed(nand_task_id, true);
         new_sym->GetHardware().GetCPUState().SetParentFirstTaskPerformed(nand_task_id, true);
